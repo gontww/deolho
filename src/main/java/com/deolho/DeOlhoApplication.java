277b6d55d3176225ee5ce9ae410c8e739e55470c
Augusto Gontijo <augusto-gontijo@outlook.com>
2026-07-09 11:28:51 -0300

    feat: initialize project configuration, application properties and entry point

---
+++ src/main/java/com/deolho/DeOlhoApplication.java
@@ -0,0 +1,12 @@
+package com.deolho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeOlhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeOlhoApplication.class, args);
+    }
+}
